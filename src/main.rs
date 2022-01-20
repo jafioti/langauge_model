05a281cd3f33770c9b630f5d797559f8e64afe8e
Joe Fioti <jafioti@gmail.com>
2022-01-20 15:21:17 -0500

    Updated to work with dataflow

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-mod data;
 use clap::Clap;
 use condor::modules::{NNModule, LanguageModel};
 use condor::utils::{ExponentialAverage, count_parameters, readable_number, train_progress_bar, test_progress_bar, sample_1d};
-use mako::{dataloader::ThreadedDataloader, tokenization::{self, Tokenizer, WordpieceTokenizer}, vocab::{Vocab, load_wordpiece_vocab}};
-use data::{LoadingState, loading_function, sorting_function, collate_function};
+use dataflow::dataloader::Dataloader;
+use dataflow::pipeline::{RandomLoader, Stateful, Node, Sort, Batch};
+use dataflow::tokenization::{WordpieceTokenizer, Tokenizer};
+use dataflow::vocab::{Vocab, WordPieceVocab};
 use tch::IndexOp;
 use tch::{Device, Kind, Tensor, nn::{self, AdamW, Optimizer, OptimizerConfig}};
 use text_io::read;
@@ -31,52 +32,77 @@ fn main() {
 fn train(load: bool) {
     // Create dataset
     println!("Builing Dataset...");
-    let mut train_dataset = ThreadedDataloader::new(
-        &["/home/jafioti/Datasets/wiki/lm_dataset.txt"],
-        BATCH_SIZE,
-        None,
-        None,
-        20000,
-        Some(LoadingState {
-            tokenizer: WordpieceTokenizer::load(),
-            vocab: load_wordpiece_vocab()
-        }),
-        loading_function,
-        Some(sorting_function),
-        None
-    );
+    let pipeline = RandomLoader::new(vec!["/home/jafioti/Datasets/wiki/lm_dataset.txt".to_string()])
+        .add_node(Stateful::new(
+            |strings: Vec<String>, (tokenizer, vocab)| {
+                let strings = strings.into_iter().map(|s| s.replace("\\", "").replace("/", "").replace('"', "")).collect();
+                let tokens = tokenizer.batch_tokenize(strings);
+                let indexes = vocab.batch_indexes_from_tokens(&tokens).unwrap();
+                indexes.into_iter().map(|indexes| {
+                    indexes[..64.min(indexes.len())].iter().map(|i| *i as i32).collect()
+                }).collect()
+            },
+            (dataflow::tokenization::WordpieceTokenizer::load(), dataflow::vocab::WordPieceVocab::load())
+        )).add_node(Sort::new(
+            |a: &Vec<i32>, b| a.len().cmp(&b.len())
+        )).add_node(Batch::new(BATCH_SIZE))
+        .add_fn(|indexes: Vec<Vec<Vec<i32>>>| {
+            indexes.into_iter().map(|batch| {
+                let (mut inputs, mut targets): (Vec<Vec<i32>>, Vec<Vec<i32>>) = batch.into_iter()
+                    .map(|indexes| (indexes[..indexes.len() - 1].to_vec(), indexes[1..].to_vec())).unzip();
+                let max_inputs = inputs.iter().map(|i| i.len()).max().unwrap();
+                inputs.iter_mut().for_each(|i| i.extend(vec![0; max_inputs - i.len()].iter()));
+                let max_targets = targets.iter().map(|i| i.len()).max().unwrap();
+                targets.iter_mut().for_each(|i| i.extend(vec![0; max_targets - i.len()].iter()));
+                (Tensor::of_slice2(&inputs), Tensor::of_slice2(&targets))
+            }).collect()
+        });
+    let mut train_dataset = Dataloader::new(pipeline)
+        .load_block_size(10_000);
+    let pipeline = RandomLoader::new(vec!["/home/jafioti/Datasets/wiki/lm_valid_dataset.txt".to_string()])
+        .add_node(Stateful::new(
+            |strings: Vec<String>, (tokenizer, vocab)| {
+                let strings = strings.into_iter().map(|s| s.replace("\\", "").replace("/", "").replace('"', "")).collect();
+                let tokens = tokenizer.batch_tokenize(strings);
+                let indexes = vocab.batch_indexes_from_tokens(&tokens).unwrap();
+                indexes.into_iter().map(|indexes| {
+                    indexes[..64.min(indexes.len())].iter().map(|i| *i as i32).collect()
+                }).collect()
+            },
+            (dataflow::tokenization::WordpieceTokenizer::load(), dataflow::vocab::WordPieceVocab::load())
+        )).add_node(Sort::new(
+            |a: &Vec<i32>, b| a.len().cmp(&b.len())
+        )).add_node(Batch::new(BATCH_SIZE))
+        .add_fn(|indexes: Vec<Vec<Vec<i32>>>| {
+            indexes.into_iter().map(|batch| {
+                let (mut inputs, mut targets): (Vec<Vec<i32>>, Vec<Vec<i32>>) = batch.into_iter()
+                    .map(|indexes| (indexes[..indexes.len() - 1].to_vec(), indexes[1..].to_vec())).unzip();
+                let max_inputs = inputs.iter().map(|i| i.len()).max().unwrap();
+                inputs.iter_mut().for_each(|i| i.extend(vec![0; max_inputs - i.len()].iter()));
+                let max_targets = targets.iter().map(|i| i.len()).max().unwrap();
+                targets.iter_mut().for_each(|i| i.extend(vec![0; max_targets - i.len()].iter()));
+                (Tensor::of_slice2(&inputs), Tensor::of_slice2(&targets))
+            }).collect()
+        });
+    let mut test_dataset = Dataloader::new(pipeline)
+        .load_block_size(10_000);
 
-    let mut test_dataset = ThreadedDataloader::new(
-        &["/home/jafioti/Datasets/wiki/lm_valid_dataset.txt"],
-        BATCH_SIZE,
-        None,
-        None,
-        1000,
-        Some(LoadingState{
-            tokenizer: WordpieceTokenizer::load(),
-            vocab: load_wordpiece_vocab()
-        }),
-        loading_function,
-        Some(sorting_function),
-        None
-    );
+    
     println!("Training Examples: {}", train_dataset.len());
     println!("Testing Examples: {}", test_dataset.len());
 
     println!("Building Model...");
-    let local_vocab = load_wordpiece_vocab();
+    let local_vocab = WordPieceVocab::load();
     let device = Device::cuda_if_available();
     let mut vs = nn::VarStore::new(device);
-    let mut model = LanguageModel::new(&vs.root(), EMBED_SIZE, HEADS, LAYERS, local_vocab.num_tokens as i64, 1000, DROPOUT);
-    if load {
-        if vs.load("model.pt").is_err() {
-            println!("Failed to load model!");
-        }
+    let mut model = LanguageModel::new(&vs.root(), EMBED_SIZE, HEADS, LAYERS, local_vocab.len() as i64, condor::modules::PositionalEncoding::Learned, 1000, DROPOUT);
+    if load && vs.load("model.pt").is_err() {
+        println!("Failed to load model!");
     }
     let mut opt = nn::AdamW::default().build(&vs, LEARNING_RATE).expect("Failed to build optimizer");
     println!("Model Parameters: {}", readable_number(count_parameters(&vs) as i64));
 
-    let tokenizer = tokenization::WordpieceTokenizer::load();
+    let tokenizer = dataflow::tokenization::WordpieceTokenizer::load();
     let mut best_loss = f64::MAX;
     for epoch in 0..EPOCHS {
         println!("\nEpoch {}", epoch + 1);
@@ -102,11 +128,11 @@ fn train(load: bool) {
 fn test() {
     // Load model
     println!("Loading Model...");
-    let local_vocab = load_wordpiece_vocab();
+    let local_vocab = WordPieceVocab::load();
     let tokenizer = WordpieceTokenizer::load();
     let device = Device::cuda_if_available();
     let mut vs = nn::VarStore::new(device);
-    let mut model = LanguageModel::new(&(&vs.root() / "lm"), EMBED_SIZE, HEADS, LAYERS, local_vocab.num_tokens as i64, 1000, DROPOUT);
+    let mut model = LanguageModel::new(&(&vs.root() / "lm"), EMBED_SIZE, HEADS, LAYERS, local_vocab.len() as i64, condor::modules::PositionalEncoding::Learned, 1000, DROPOUT);
     vs.load("model.pt").expect("Failed to load model parameters!");
     println!("Model Parameters: {}", readable_number(count_parameters(&vs) as i64));
     println!("\n----------------------------------------------");
@@ -120,19 +146,19 @@ fn test() {
     }
 }
 
-fn train_epoch<T: Tokenizer>(model: &mut LanguageModel, dataset: &mut ThreadedDataloader<Vec<u32>, LoadingState<T>>, optimizer: &mut Optimizer<AdamW>) -> (f64, f64) {
+fn train_epoch<N: Node<Input = (), Output = (Tensor, Tensor)> + Send + 'static>(model: &mut LanguageModel, dataset: &mut Dataloader<N>, optimizer: &mut Optimizer<AdamW>) -> (f64, f64) 
+where N::Output: Send {
     model.train();
     let bar = train_progress_bar((dataset.len() / BATCH_SIZE) as u64);
     let mut loss_avg = ExponentialAverage::new();
     let mut accuracies: Vec<f64> = vec![];
     
-    for (i, batch) in dataset.enumerate() {
+    for (i, (mut inputs, mut targets)) in dataset.enumerate() {
         optimizer.zero_grad();
         // Process batch into tensors
-        let (mut inputs, mut targets) = collate_function(batch);
-        let (batch_size, seq_length) = inputs.size2().unwrap();
         inputs = inputs.to_device(Device::cuda_if_available());
         targets = targets.to_device(Device::cuda_if_available());
+        let (batch_size, seq_length) = inputs.size2().unwrap();
         // Run through model
         let output = model.forward(&inputs);
 
@@ -155,14 +181,14 @@ fn train_epoch<T: Tokenizer>(model: &mut LanguageModel, dataset: &mut ThreadedDa
     (loss_avg.value, accuracies.iter().sum::<f64>() / accuracies.len() as f64)
 }
 
-fn test_epoch<T: Tokenizer>(model: &mut LanguageModel, dataset: &mut ThreadedDataloader<Vec<u32>, LoadingState<T>>) -> (f64, f64) {
+fn test_epoch<N: Node<Input = (), Output = (Tensor, Tensor)> + Send + 'static>(model: &mut LanguageModel, dataset: &mut Dataloader<N>) -> (f64, f64)
+where N::Output: Send {
     model.eval();
     let mut losses = vec![];
     let mut accuracies: Vec<f64> = vec![];
     let bar = test_progress_bar((dataset.len() / BATCH_SIZE) as u64);
-    for batch in dataset {
+    for (mut inputs, mut targets) in dataset {
         // Process batch into tensors
-        let (mut inputs, mut targets) = collate_function(batch);
         let (batch_size, seq_length) = inputs.size2().unwrap();
         inputs = inputs.to_device(Device::cuda_if_available());
         targets = targets.to_device(Device::cuda_if_available());
@@ -181,7 +207,7 @@ fn test_epoch<T: Tokenizer>(model: &mut LanguageModel, dataset: &mut ThreadedDat
     (losses.iter().sum::<f64>() / losses.len() as f64, accuracies.iter().sum::<f64>() / accuracies.len() as f64)
 }
 
-fn generate<T: Tokenizer>(sentence: String, model: &mut LanguageModel, tokens_to_generate: usize, temperature: f64, local_vocab: &Vocab, tokenizer: &T) -> String {
+fn generate<T: Tokenizer, V: Vocab>(sentence: String, model: &mut LanguageModel, tokens_to_generate: usize, temperature: f64, local_vocab: &V, tokenizer: &T) -> String {
     model.eval();
     let mut tokens = tokenizer.tokenize(&sentence);
     for _ in 0..tokens_to_generate {
@@ -192,7 +218,7 @@ fn generate<T: Tokenizer>(sentence: String, model: &mut LanguageModel, tokens_to
         let input = Tensor::of_slice(&vectorized).unsqueeze(0).to(Device::cuda_if_available());
         let output = model.forward(&input);
         let index = sample_1d(output.squeeze_dim(0).i(-1), temperature);
-        let token = local_vocab.index2token[index].clone();
+        let token = local_vocab.tokens_from_indexes(&[index]).unwrap().pop().unwrap();
         tokens.push(token);
     }
     tokenizer.untokenize(tokens)
